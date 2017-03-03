33dc171f474aed80a93a1e872f725ffddcb3b3d8
Vijay Tholpadi <vijay@thegeekprojekt.com>
2017-03-03 20:04:36 +0530

    [WIP] Thinning down the functionality of the Networking parts

--- VTNetworkHelper/VTNetworkingHelper.h
+++ VTNetworkHelper/VTNetworkingHelper.h
@@ -7,35 +7,36 @@
 
 #import <Foundation/Foundation.h>
 #import "AFNetworking.h"
+
+//Models
 #import "VTNetworkResponse.h"
 
-#import "VTNoInternetView.h"
-
 typedef void (^VTNetworkRequestCompletionHandler) (VTNetworkResponse * response);
-//typedef void (^BIError) (NSError *error);
 
 @interface VTNetworkingHelper : NSObject
-
-// Base server URL relative to which all other URLs will be called
-@property (nonatomic, strong) NSString *serverURLPath;
-@property (nonatomic, strong) NSURL *serverURL;
-@property (nonatomic, assign) BOOL isInternetViewVisible;
-@property (nonatomic, strong) VTNoInternetView *noInternetView;
 
 // Network operation functions
 /*
 - For GET requests the performRequestWithPath should be called as there is
  no need to send parameters.
+*/
+- (void)performRequestWithPath:(NSString *)path
+                      withAuth:(BOOL)needsAuth
+     withRequestJSONSerialized:(BOOL)reqJSONSerialized
+         withCompletionHandler:(VTNetworkRequestCompletionHandler)handler;
+
+/*
 - For POST, PUT and DELETE requests the performRequestWithPath:withParams needs
  to be called with the necessary parameters.
  */
-- (void)performRequestWithPath:(NSString *)path withAuth:(BOOL)needsAuth withRequestJSONSerialized:(BOOL)reqJSONSerialized withCompletionHandler:(VTNetworkRequestCompletionHandler)handler;
-
-- (void)performRequestWithPath:(NSString *)path withAuth:(BOOL)needsAuth forMethod:(NSString *)method withRequestJSONSerialized:(BOOL)reqJSONSerialized withParams:(id)params withCompletionHandler:(VTNetworkRequestCompletionHandler)handler;
-
+- (void)performRequestWithPath:(NSString *)path
+                      withAuth:(BOOL)needsAuth
+                     forMethod:(NSString *)method
+     withRequestJSONSerialized:(BOOL)reqJSONSerialized
+                    withParams:(id)params
+         withCompletionHandler:(VTNetworkRequestCompletionHandler)handler;
 
 //Class methods - Shared Instance
 + (VTNetworkingHelper *)sharedInstance;
-- (AFNetworkReachabilityStatus)getReachabilityStatus;
 
 @end
